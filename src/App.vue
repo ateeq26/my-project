0701ae66878a45e3e41cbc21a7373699de6b601e
ateeq26 <ateeq26@outlook.com>
2023-02-11 10:54:24 +0530

    Initialized

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <button @click="getApiData">POST Data to API</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    async getApiData() {
+      var payload = {
+        app_name: "prodfda",
+        uid: "some uid2",
+        token:
+          "e1gPO7MNT8qzm_Fs2Ny17g:APA91bF1CXKkLU3sI9KeW8pW7g-dYmOzqQzDgcOMbiYdiIm2zpmsv3cUS7ZF7byW-s5HITt2GB7L5rVH_ncGRxwRhUGOXEGKxtPtZQGQnWlX_cCourmrONqQ6XIf9PYDWjQM3aMRp-Xw",
+        email: "test123@grr.la",
+      };
+      console.log("notification payload", payload);
+      var response = await axios.post(
+        `https://notificationservice.in/fcm/register`,
+        payload
+      );
+      console.log(response.data);
+    },
+  },
+};
 </script>
 
 <style>
